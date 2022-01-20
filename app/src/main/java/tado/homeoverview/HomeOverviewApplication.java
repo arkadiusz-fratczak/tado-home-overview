a4335b661a7238893365961961a0dade54099a6e
Arkadiusz Fratczak <arkadiusz.ef@gmail.com>
2022-01-20 19:04:55 +0100

    Design of Home Overview API. Application skeleton

---
+++ app/src/main/java/tado/homeoverview/HomeOverviewApplication.java
@@ -0,0 +1,12 @@
+package tado.homeoverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeOverviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeOverviewApplication.class, args);
+    }
+}
